f69adbc7d86485718c973e5983b969fd246c18d1
Rihards Fridrihsons <rfridrihsons@gmail.com>
2016-11-19 19:22:20 +0200

    Add dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  static css()
+  {
+    return `
+
+    `;
+  }
+  render () {
+    return (
+      <div>
+        Dashboard
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
